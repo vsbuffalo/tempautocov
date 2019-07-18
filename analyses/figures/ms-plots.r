3ef1a2815bc11b2952d7f64bd16e65f4cbe4570c
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2019-07-18 15:34:44 -0700

    update to figures; code

--- analyses/figures/ms-plots.r
+++ analyses/figures/ms-plots.r
@@ -435,6 +435,51 @@ dev.off()
 
 
 
+#pdf('mom-fits-N-Va.pdf', width=textwidth/2, height=textwidth/2)
+#wescols <- wesanderson::wes_palette('Zissou1')
+#wescols <- brewer.pal(5,"Dark2")
+#mom_fitsd_subset <- mom_fitsd %>% 
+#  filter(Va %in% c(0.001, 0.005, 0.01, 0.05, 0.1)) #%>%
+#  #filter(genlen %in% c(0.1, 0.5, 1.5))
+#boxplot(Nest ~ Va + genlen, mom_fitsd_subset,
+#        axes=FALSE, ylim=c(0, 1.5e3), outline=FALSE,
+#        #col=wescols,
+#        border=wescols,
+#        pars = list(boxcol = "transparent", medlty = "blank", 
+#                    medpch=16, whisklty = c(1, 1),
+#                    medcex = 0.5, outcex = 0, staplelty = "blank"))
+#abline(h=1e3, col=dashed_gray, lty=2)
+#x <- 1:20
+#y <- mom_fitsd_subset %>% group_by(Va, genlen) %>% 
+#  summarize(Ne_est=mean(Ne_est, na.rm=TRUE)) %>%
+#  arrange(genlen, Va) %>% pull(Ne_est)
+#points(x, y, pch='-', col=wescols)
+#axis(1, at=c(1, 5), labels=c("", "0.01"),
+#     hadj=3,
+#     padj=-2.4,
+#     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#axis(1, at=c(6, 10), 
+#     labels=c("", "0.1"), hadj=4,
+#     padj=-2.4,
+#     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+
+#axis(1, at=c(11, 15), 
+#     labels=c("", "0.5"), hadj=4,
+#     padj=-2.4,
+#     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#axis(1, at=c(16, 20), 
+#     labels=c("", "1.5"), hadj=4,
+#     padj=-2.4,
+#     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#axis(2, at=c(0, 500, 1e3, 1.5e3), 
+#     tck=0.018, hadj=0.4, las=1, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#mtext(latex2exp:::TeX('level of recombination (Morgans)'), 
+#      1, line=1.2, cex=mtext_cex)
+#mtext(latex2exp:::TeX('estimated N'), 2, line=2, cex=mtext_cex)
+#dev.off()
+
+
+# The original standalone estimated N figure
 pdf('mom-fits-N.pdf', width=textwidth/2, height=textwidth/2)
 boxplot(Nest ~ genlen, mom_fitsd, axes=FALSE, ylim=c(0, 2e3), outline=FALSE,
         pars = list(boxcol = "transparent", medlty = "blank", 
@@ -454,65 +499,66 @@ dev.off()
 
 
 
-## Estimation Figure -- both in one
-pdf('mom-fits-both.pdf', width=textwidth, height=textwidth/2 * 0.9)
 
-layout(matrix(c(1, 2, 3, 4), ncol=2, byrow=TRUE), heights=c(0.1, 0.90))
-opar <- par(no.readonly=TRUE)
-par(mar=c(0,0,0,0), xpd=TRUE)
-plot.new()
-text(0.1, 0.3, "A", cex=1.5)
+## Estimation Figure -- both Va and N estimation, with N boxplot grouped by Va
+#pdf('mom-fits-both.pdf', width=textwidth, height=textwidth/2 * 0.9)
 
-plot.new()
-text(0.1, 0.3, "B", cex=1.5)
+#layout(matrix(c(1, 2, 3, 4), ncol=2, byrow=TRUE), heights=c(0.1, 0.90))
+#opar <- par(no.readonly=TRUE)
+#par(mar=c(0,0,0,0), xpd=TRUE)
+#plot.new()
+#text(0.1, 0.3, "A", cex=1.5)
 
-data(mom_fitsd)
-wescols <- wesanderson::wes_palette('Darjeeling1')[c(4, 1, 2, 3, 5)]
-mom_fitsd$genlen <- droplevels(mom_fitsd$genlen)
+#plot.new()
+#text(0.1, 0.3, "B", cex=1.5)
 
-par(mar=c(4.1, 3.1, 0.1, 2.1))
-with(mom_fitsd[mom_fitsd$va0_est > 1e-4, ],
-     plot(emp_va, va0_est, col=wescols[as.factor(genlen)],
-          xlab='', ylab='',
-          pch=19, cex=0.5, axes=FALSE,
-          ylim=c(1e-4, 10),
-          xlim=c(1e-4, 1),
-          lwd=0, log='xy'))
-#abline(a=0, b=1, col=dashed_gray, lty=2)
-segments(1e-4, 1e-4, 1, 1, col=dashed_gray, lty=2)
-axes_col <- 'gray12'
-axis_cex <- 1*0.7
-axis(1, at=10^(-(4:0)), labels=pretty_log(-(4:0)), 
-     padj=-1.5,
-     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
-axis(2, at=10^(c(-(4:0), 1)), labels=pretty_log(c(-(4:0), 1)), 
-     tck=0.018, hadj=0.4, las=1, col=axes_col, lwd=1.2, cex.axis=axis_cex)
-mtext(latex2exp:::TeX('empirical $V_A(1)$'), 1, line=1.3, cex=mtext_cex)
-mtext(latex2exp:::TeX('estimated $V_A(1)$'), 2, line=1.5, cex=mtext_cex)
-legend(1e-4, 5, inset=0, legend=unique(mom_fitsd$genlen), fill=wescols,
-           title=latex2exp:::TeX('level of\nrecombination (Morgans)'),
-           cex=0.6,
-           ncol=2,
-           bty='n', text.col=title_col,
-           border=0)
-#text(1e-4, 11, 'A', line=2, cex=2, xpd=TRUE)
+#data(mom_fitsd)
+#wescols <- wesanderson::wes_palette('Darjeeling1')[c(4, 1, 2, 3, 5)]
+#mom_fitsd$genlen <- droplevels(mom_fitsd$genlen)
 
-boxplot(Nest ~ genlen, mom_fitsd, axes=FALSE, ylim=c(0, 2e3), outline=FALSE,
-        pars = list(boxcol = "transparent", medlty = "blank", 
-                    medpch=16, whisklty = c(1, 1),
-                    medcex = 0.7,  outcex = 0, staplelty = "blank"))
-#abline(h=1e3, col=dashed_gray, lty=2)
-segments(0.3, 1e3, 4.4, 1e3, col=dashed_gray, lty=2)
-axis(1, at=unique(mom_fitsd$genlen), labels=levels(mom_fitsd$genlen), 
-     padj=-2.4,
-     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
-axis(2, at=c(0, 500, 1e3, 1.5e3, 2e3), 
-     tck=0.018, hadj=0.4, las=1, col=axes_col, lwd=1.2, cex.axis=axis_cex)
-mtext(latex2exp:::TeX('level of recombination (Morgans)'), 
-      1, line=1.2, cex=mtext_cex)
-mtext(latex2exp:::TeX('estimated N'), 2, line=2, cex=mtext_cex)
-#text(0, 2010, 'B', line=2, cex=2, xpd=TRUE)
-dev.off()
+#par(mar=c(4.1, 3.1, 0.1, 2.1))
+#with(mom_fitsd[mom_fitsd$va0_est > 1e-4, ],
+#     plot(emp_va, va0_est, col=wescols[as.factor(genlen)],
+#          xlab='', ylab='',
+#          pch=19, cex=0.5, axes=FALSE,
+#          ylim=c(1e-4, 10),
+#          xlim=c(1e-4, 1),
+#          lwd=0, log='xy'))
+##abline(a=0, b=1, col=dashed_gray, lty=2)
+#segments(1e-4, 1e-4, 1, 1, col=dashed_gray, lty=2)
+#axes_col <- 'gray12'
+#axis_cex <- 1*0.7
+#axis(1, at=10^(-(4:0)), labels=pretty_log(-(4:0)), 
+#     padj=-1.5,
+#     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#axis(2, at=10^(c(-(4:0), 1)), labels=pretty_log(c(-(4:0), 1)), 
+#     tck=0.018, hadj=0.4, las=1, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#mtext(latex2exp:::TeX('empirical $V_A(1)$'), 1, line=1.3, cex=mtext_cex)
+#mtext(latex2exp:::TeX('estimated $V_A(1)$'), 2, line=1.5, cex=mtext_cex)
+#legend(1e-4, 5, inset=0, legend=unique(mom_fitsd$genlen), fill=wescols,
+#           title=latex2exp:::TeX('level of\nrecombination (Morgans)'),
+#           cex=0.6,
+#           ncol=2,
+#           bty='n', text.col=title_col,
+#           border=0)
+##text(1e-4, 11, 'A', line=2, cex=2, xpd=TRUE)
+
+#boxplot(Nest ~ genlen, mom_fitsd, axes=FALSE, ylim=c(0, 2e3), outline=FALSE,
+#        pars = list(boxcol = "transparent", medlty = "blank", 
+#                    medpch=16, whisklty = c(1, 1),
+#                    medcex = 0.7,  outcex = 0, staplelty = "blank"))
+##abline(h=1e3, col=dashed_gray, lty=2)
+#segments(0.3, 1e3, 4.4, 1e3, col=dashed_gray, lty=2)
+#axis(1, at=unique(mom_fitsd$genlen), labels=levels(mom_fitsd$genlen), 
+#     padj=-2.4,
+#     tck=0.01, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#axis(2, at=c(0, 500, 1e3, 1.5e3, 2e3), 
+#     tck=0.018, hadj=0.4, las=1, col=axes_col, lwd=1.2, cex.axis=axis_cex)
+#mtext(latex2exp:::TeX('level of recombination (Morgans)'), 
+#      1, line=1.2, cex=mtext_cex)
+#mtext(latex2exp:::TeX('estimated N'), 2, line=2, cex=mtext_cex)
+##text(0, 2010, 'B', line=2, cex=2, xpd=TRUE)
+#dev.off()
 
 
 ## Figure Prop var due to linked sel figure -- conservative G
